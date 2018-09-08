53762764f88370161e0b41d1e7a7c08636a0e1c8
Tim L <tblukens87@gmail.com>
2018-09-08 11:03:48 -0500

    Small changes to button text and hover

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,66 +5,66 @@ var triviaGame = {
 
     // CREATE AN ARRAY WITH QUESTIONS AND ANSWERS. ASSIGN THE CORRECT ANSWER INSIDE ARRAY
     questionsArr: [{
-        question: "Who is king of Westeros when the series begins?",
-        options: ["Aerys Targaryen", "Eddard Stark", "Robert Baratheon", "Tywin Lannister"],
-        answer: 2,
-        answerText: "",
-    },
-    {
-        question: "What noble house is Catelyn Stark from?",
-        options: ["House Tyrell", "House Tully", "House Lannister", "House Stark"],
-        answer: 1,
-        answerText: "",
-    },
-    {
-        question: "Who is the true heir to the iron throne after Robert Baratheon's death?",
-        options: ["Joffrey Baratheon", "Stannis Baratheon", "Renly Baratheon", "Eddard Stark"],
-        answer: 1,
-        answerText: "",
-    },
-    {
-        question: "What was the name of Robb Stark's wife?",
-        options: ["Talisa", "Shae", "Ros", "Mary"],
-        answer: 0,
-        answerText: "",
-    },
-    {
-        question: "What is the name of the novels the show Game of Thrones is based off of?",
-        options: ["Dances With Wolves", "The Wheel of Time", "Everybody Poops", "A Song of Ice and Fire"],
-        answer: 3,
-        answerText: "",
-    },
-    {
-        question: "What noble house has a sigil of a silver trout?",
-        options: ["House Tully", "House Greyjoy", "House Stark", "House Tyrell"],
-        answer: 0,
-        answerText: "",
-    },
-    {
-        question: "Who was Hand of the King before Eddard Stark?",
-        options: ["Tywin Lannister", "Jon Arryn", "Renly Baratheon", "Tyrion Lannister"],
-        answer: 1,
-        answerText: "",
-    },
-    {
-        question: "How did Daenerys Targaryen eventually hatch her dragon eggs?",
-        options: ["In a lightning storm", "In a funeral pyre", "In a fireplace", "In a frozen cave"],
-        answer: 1,
-        answerText: "",
-    },
-    {
-        question: "The phrase 'Valar Morghulis' or 'all men must die' is usually responded with:",
-        options: ["Valar Dohaeris or 'all men must serve'", "Valar Rohnas or 'all men must live'", "Valar GoGo or 'all men must dance'"],
-        answer: 0,
-        answerText: "",
-    },
-    {
-        question: "Who is Joffrey, Myrcella and Tommen's real father?",
-        options: ["Lancel Lannister", "Robert Baratheon", "Jaime Lannister", "Tyrion Lannister"],
-        answer: 2,
-        answerText: "",
-    },
-    ],    //** POSSIBLY CREATE ARRAY TO HOLD CORRECT AND INCORRECT QUESTIONS? ?? **//
+            question: "Who is king of Westeros when the series begins?",
+            options: ["Aerys Targaryen", "Eddard Stark", "Robert Baratheon", "Tywin Lannister"],
+            answer: 2,
+            answerText: "",
+        },
+        {
+            question: "What noble house is Catelyn Stark from?",
+            options: ["House Tyrell", "House Tully", "House Lannister", "House Stark"],
+            answer: 1,
+            answerText: "",
+        },
+        {
+            question: "Who is the true heir to the iron throne after Robert Baratheon's death?",
+            options: ["Joffrey Baratheon", "Stannis Baratheon", "Renly Baratheon", "Eddard Stark"],
+            answer: 1,
+            answerText: "",
+        },
+        {
+            question: "What was the name of Robb Stark's wife?",
+            options: ["Talisa", "Shae", "Ros", "Mary"],
+            answer: 0,
+            answerText: "",
+        },
+        {
+            question: "What is the name of the novels the show Game of Thrones is based off of?",
+            options: ["Dances With Wolves", "The Wheel of Time", "Everybody Poops", "A Song of Ice and Fire"],
+            answer: 3,
+            answerText: "",
+        },
+        {
+            question: "What noble house has a sigil of a silver trout?",
+            options: ["House Tully", "House Greyjoy", "House Stark", "House Tyrell"],
+            answer: 0,
+            answerText: "",
+        },
+        {
+            question: "Who was Hand of the King before Eddard Stark?",
+            options: ["Tywin Lannister", "Jon Arryn", "Renly Baratheon", "Tyrion Lannister"],
+            answer: 1,
+            answerText: "",
+        },
+        {
+            question: "How did Daenerys Targaryen eventually hatch her dragon eggs?",
+            options: ["In a lightning storm", "In a funeral pyre", "In a fireplace", "In a frozen cave"],
+            answer: 1,
+            answerText: "",
+        },
+        {
+            question: "The phrase 'Valar Morghulis' or 'all men must die' is usually responded with:",
+            options: ["Valar Dohaeris or 'all men must serve'", "Valar Rohnas or 'all men must live'", "Valar GoGo or 'all men must dance'"],
+            answer: 0,
+            answerText: "",
+        },
+        {
+            question: "Who is Joffrey, Myrcella and Tommen's real father?",
+            options: ["Lancel Lannister", "Robert Baratheon", "Jaime Lannister", "Tyrion Lannister"],
+            answer: 2,
+            answerText: "",
+        },
+    ], //** POSSIBLY CREATE ARRAY TO HOLD CORRECT AND INCORRECT QUESTIONS? ?? **//
     // CREATE VARIABLES TO TRACK CORRECT, WRONG, AND UNANSWERED COUNT
     currentQuestionIndex: 0,
     correct: 0,
@@ -87,6 +87,7 @@ var triviaGame = {
                     type: "button",
                     id: [i],
                     name: element,
+                    value: element
                 })
                 newOptionButton.addClass("btn btn-dark btn-lg border border-light btn-block w-75 m-auto");
                 newOptionButton.text(element);
@@ -96,16 +97,26 @@ var triviaGame = {
             }
             var answer = parseInt(this.questionsArr[this.currentQuestionIndex].answer);
 
+
+            $("button").mouseenter(function () {
+                if ($(this).attr('value') !== "RESET") {
+                    var val = $(this).attr('value');
+                    $(this).html("<i class='fas fa-shield-alt'></i>  " + val + "  <i class='fas fa-shield-alt'></i>")
+                }
+            })
+            $("button").mouseleave(function () {
+                if ($(this).attr('value') !== "RESET") {
+                    var val = $(this).attr('value');
+                    $(this).html(val)
+                }
+            })
             $("button").on("click", function () {
-                console.log(triviaGame.questionsArr[triviaGame.currentQuestionIndex].answer);
                 var anwserCheck = parseInt(this.id);
                 if (anwserCheck === answer) {
-                    console.log("Correct")
                     triviaGame.currentQuestionIndex++
                     triviaGame.correct++
                     triviaGame.loadQuestion();
                 } else {
-                    console.log("Incorrect")
                     triviaGame.currentQuestionIndex++
                     triviaGame.incorrect++
                     triviaGame.loadQuestion();
@@ -128,7 +139,7 @@ var triviaGame = {
         this.correct = 0;
         this.incorrect = 0;
 
-        $("#START").text("RESET").detach().show().appendTo("#the-quiz");
+        $("#START").attr('value', "RESET").text("RESET").detach().show().appendTo("#the-quiz");
         $("#START").on("click", function () {
             $("#START").detach().hide().appendTo("#start-hide");
             triviaGame.loadQuestion();
@@ -150,26 +161,26 @@ $(document).ready(
 
 );
 
-window.onload = function() {    
+window.onload = function () {
 
-	var theWindow        = $(window),
-	    $bg              = $("#bg"),
-	    aspectRatio      = $bg.width() / $bg.height();
-	    			    		
-	function resizeBg() {
-		
-		if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
-		    $bg
-		    	.removeClass()
-		    	.addClass('bgheight');
-		} else {
-		    $bg
-		    	.removeClass()
-		    	.addClass('bgwidth');
-		}
-					
-	}
-	                   			
-	theWindow.resize(resizeBg).trigger("resize");
+    var theWindow = $(window),
+        $bg = $("#bg"),
+        aspectRatio = $bg.width() / $bg.height();
+
+    function resizeBg() {
+
+        if ((theWindow.width() / theWindow.height()) < aspectRatio) {
+            $bg
+                .removeClass()
+                .addClass('bgheight');
+        } else {
+            $bg
+                .removeClass()
+                .addClass('bgwidth');
+        }
+
+    }
+
+    theWindow.resize(resizeBg).trigger("resize");
 
 };

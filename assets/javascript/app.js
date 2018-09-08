89b2bb8f571e8c36e09443be69a8fb174464aa2a
Tim Lukens <tblukens87@gmail.com>
2018-09-07 22:03:40 -0500

    Changed background image size using javascript function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -149,3 +149,27 @@ $(document).ready(
 
 
 );
+
+window.onload = function() {    
+
+	var theWindow        = $(window),
+	    $bg              = $("#bg"),
+	    aspectRatio      = $bg.width() / $bg.height();
+	    			    		
+	function resizeBg() {
+		
+		if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+		    $bg
+		    	.removeClass()
+		    	.addClass('bgheight');
+		} else {
+		    $bg
+		    	.removeClass()
+		    	.addClass('bgwidth');
+		}
+					
+	}
+	                   			
+	theWindow.resize(resizeBg).trigger("resize");
+
+};
